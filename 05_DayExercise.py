9559c14354d50cfdafaff847978110d7d3854df0
UP240890 <up240890@alumnos.upa.edu.mx>
2025-03-26 15:35:04 -0600

    Add files via upload

---
+++ 05_DayExercise.py
@@ -0,0 +1,28 @@
+#exercise1#
+lst = list()
+#exercise2#
+sopa=['agua','sal','pasta','verdura','pollo']
+#exercise3#
+print(len(sopa))
+#exercise4#
+sopamid=len(sopa) // 2
+print(sopa[0], sopa[sopamid], sopa[-1]) 
+#exercise5#
+MDT=['Jesus', '19', '1.72', 'solo', 'casa'] #MDT=mixed_data_types#
+#exercise6#
+it_companies=['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+#exercise7#
+print(it_companies)
+#exercise8#
+print(len(it_companies))
+#exercise9#
+it_companiesmid=len(it_companies) // 2
+print(it_companies[0],it_companies[it_companiesmid],it_companies[-1])  
+#exersie10#
+it_companies[0]="Ponos"
+print(it_companies)
+#exercise11#
+it_companies.append("Tesla")
+print(it_companies)
+#exercise12#
+
